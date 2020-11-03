1a222bcde41312ee20df27474e3c4f9d963c8549
HoangLan2904 <maivanhoanglan@gmail.com>
2020-11-03 12:57:50 +0700

    AlgorithmModel completed checkHorizontalRect 2, checkHorizontalRec3

---
+++ core/src/com/ss/OfficialPackage/configs/BoardConfig.java
@@ -0,0 +1,7 @@
+package com.ss.OfficialPackage.configs;
+
+public class BoardConfig {
+
+  public static int width = 17;
+  public static int height = 8;
+}
--- core/src/com/ss/OfficialPackage/models/AlgorithmModel.java
+++ core/src/com/ss/OfficialPackage/models/AlgorithmModel.java
@@ -2,6 +2,7 @@ package com.ss.OfficialPackage.models;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.ss.OfficialPackage.configs.BoardConfig;
 
 public class AlgorithmModel {
 
@@ -103,6 +104,112 @@ public class AlgorithmModel {
       }
     }
     path.removeRange(0, path.size -1);
+    return path;
+  }
+
+  // kiểm tra theo chiều ngang 2
+  private static Array<Vector2> checkHorizontalRect2(AnimalModel animal1, AnimalModel animal2, Array<Array<AnimalModel>> animals) {
+    //B1: duyet theo chieu ngang, lan luot ve den widthBoard
+    Array<Vector2> path = new Array<>();
+    path.add(new Vector2(animal1.getRow(), animal1.getCol()));
+
+    for(int i = animal1.getCol() + 1; i <= BoardConfig.width; i++) {
+      //neu i == BoardConfig.width bo qua duyet theo chieu doc(B2), duyet den chieu ngang (B3)
+      if(i == BoardConfig.width){
+        path.add(new Vector2(animal1.getRow(), BoardConfig.width));
+        path.add(new Vector2(animal2.getRow(), BoardConfig.width));
+
+        int col = animal2.getCol() + 1;
+        int officialCol = Math.min(col, BoardConfig.width - 1);
+        AnimalModel ani1 = animals.get(animal2.getRow()).get(BoardConfig.width -1);
+        AnimalModel ani2 = animals.get(animal2.getRow()).get(officialCol);
+        if (col == BoardConfig.width || checkRow(ani1, ani2, animals)){
+          path.add(new Vector2(animal2.getRow(), animal2.getCol()));
+          System.out.println("path: " + path);
+          return path;
+        }
+        else {
+          path.removeRange(0, path.size -1);
+          return path;
+        }
+      }
+      else {
+        //B2: duyet theo chieu doc, xuong row ani2
+        if(animals.get(animal1.getRow()).get(i).getId() == -1){
+          if(i > animal2.getCol()){
+            path.add(new Vector2(animal1.getRow(), i));
+
+            AnimalModel ani1 = animals.get(animal1.getRow()).get(i);
+            AnimalModel ani2 = animals.get(animal2.getRow()).get(i);
+            boolean checkCol = checkCol(ani1, ani2, animals);
+            if(checkCol){
+              //B3: duyet theo chieu ngang, den col ani2 + 1
+              path.add(new Vector2(animal2.getRow(), i));
+
+              int col = Math.min(animal2.getCol() + 1, BoardConfig.width -1);
+              AnimalModel aniR1 = ani2;
+              AnimalModel aniR2 = animals.get(animal2.getRow()).get(col);
+              if(checkRow(aniR1, aniR2, animals)){
+                path.add(new Vector2(animal2.getRow(), animal2.getCol()));
+                System.out.println("path: " + path);
+                return path;
+              }
+              path.removeRange(0, path.size - 1);
+              return path;
+            }
+            else path.pop();
+          }
+        }
+        else {
+          path.removeRange(0, path.size - 1);
+          return path;
+        }
+      }
+    }
+    path.removeRange(0, path.size - 1);
+    return path;
+  }
+
+  // kiểm tra theo chiều ngang 3
+  private static Array<Vector2> checkHorizontalRect3(AnimalModel animal1, AnimalModel animal2, Array<Array<AnimalModel>> animals){
+    Array<Vector2> path = new Array<>();
+    path.add(new Vector2(animal1.getRow(), animal1.getCol()));
+
+    boolean logic = animal1.getCol() < animal2.getCol();
+    int startId = logic ? animal1.getCol() + 1 : animal1.getCol() - 1;
+    int endId = logic ? animal2.getCol() - 1 : animal2.getCol() + 1;
+    int operator = logic ? 1 : -1;
+    for(int i = startId; (logic && i <= endId) || (!logic && i >= endId); i += operator){
+      if(animals.get(animal1.getRow()).get(i).getId() == -1){
+        path.add(new Vector2(animal1.getRow(), i));
+
+        AnimalModel ani1 = animals.get(animal1.getRow()).get(i);
+        AnimalModel ani2 = animals.get(animal2.getRow()).get(i);
+        boolean checkCol = checkCol(ani1, ani2, animals);
+        if(checkCol){
+          path.add(new Vector2(animal2.getRow(), i));
+
+          AnimalModel aniR1 = ani2;
+          AnimalModel aniR2 = animals.get(animal2.getRow()).get(endId);
+          boolean checkRow = checkRow(aniR1, aniR2, animals);
+          if(checkRow){
+            path.add(new Vector2(animal2.getRow(), animal2.getCol()));
+            System.out.println("Path: " + path);
+            return path;
+          }
+          else {
+            path.pop();
+            path.pop();
+          }
+        }
+        else path.pop();
+      }
+      else {
+        path.removeRange(0, path.size - 1);
+        return path;
+      }
+    }
+    path.removeRange(0, path.size - 1);
     return path;
   }
 
